b4c80e1c5c98a901a2b5068014990da2b65d2ad7
Juliana <juliana.salafia@gmail.com>
2021-05-06 16:18:31 -0300

    Exemplo do uso de Listas com Vector

--- implementando.collections.e.streams.com.java/src/list/exemplos/ExemploVector.java
+++ implementando.collections.e.streams.com.java/src/list/exemplos/ExemploVector.java
@@ -1,2 +1,33 @@
-package list.exemplos;public class ExemploVector {
+package list.exemplos;
+
+import java.util.List;
+import java.util.Vector;
+
+public class ExemploVector {
+    public static void main(String[] args) {
+        List<String> esportes = new Vector<>();
+
+        //adiciona 4 esportes no vetor
+        esportes.add("Futebol");
+        esportes.add("Basquete");
+        esportes.add("Tênis de Mesa");
+        esportes.add("Handebol");
+
+        //altera o valor da posição 2 do vetor
+        esportes.set(2, "Ping Pong");
+
+        //remove o esporte da posição 2
+        esportes.remove(2);
+
+        //remove o esporte handebol do vetor
+        esportes.remove("Handebol");
+
+        //retorna o primeiro item do vetor
+        System.out.println(esportes.get(0));
+
+        //navega nos esportes
+        for (String esporte: esportes) {
+            System.out.println(esporte);
+        }
+    }
 }
